1f5d07bc63be4ae1d385bb2bce88d5900788331d
wroclawianka <dorota.zelga@gmail.com>
2018-09-11 18:31:51 +0200

    Pie view created

---
+++ js/views/pie.js
@@ -0,0 +1,38 @@
+export default class Pie {
+  constructor(type) {
+    this.type = type;
+    this.width = 160;
+    this.height = 160;
+    this.volume = 7;
+  }
+
+  createSVG(data) {
+    const labels = Object.keys(data)
+    const values = Object.values(data);
+    const radius = Math.min(this.width, this.height) / 2;
+    const arc = d3.svg
+      .arc()
+      .innerRadius(radius - this.volume)
+      .outerRadius(radius);
+    const pie = d3.layout.pie();
+
+    const svg = d3
+      .select("body")
+      .append("svg")
+      .attr("class", this.type)
+      .attr("width", this.width)
+      .attr("height", this.height)
+      .append("g")
+      .attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")");
+
+    svg
+      .selectAll("path")
+      .data(pie(values))
+      .enter()
+      .append("path")
+      .attr("class", function(d, i) {
+        return labels[i]
+      })
+      .attr("d", arc);
+  }
+}
