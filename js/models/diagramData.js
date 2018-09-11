f3341f753cd1b6246f8cb8c4f3eacadc23c08cd4
wroclawianka <dorota.zelga@gmail.com>
2018-09-11 18:18:42 +0200

    order of the class attributes changed

--- js/models/diagramData.js
+++ js/models/diagramData.js
@@ -1,5 +1,5 @@
 export default class DiagramData {
   constructor(tablet, smartphone) {
-    (this.tablet = tablet), (this.smartphone = smartphone);
+    (this.smartphone = smartphone), (this.tablet = tablet);
   }
 }
--- js/models/impresions.js
+++ js/models/impresions.js
@@ -1,7 +1,7 @@
 import DiagramData from "./diagramData.js";
 
 export default class Impresions extends DiagramData {
-  constructor(tablet, smartphone) {
-    super(tablet, smartphone);
+  constructor(smartphone, tablet) {
+    super(smartphone, tablet);
   }
 }
