12aa8743b89aa1a71cd4c9f475b1441304185743
wroclawianka <dorota.zelga@gmail.com>
2018-09-11 09:05:26 +0200

    fetchData function created

---
+++ js/services/apiService.js
@@ -0,0 +1,18 @@
+export default class ApiService {
+  constructor(apiUrl) {
+    this.apiUrl = apiUrl;
+  }
+
+  fetchData(data) {
+    const url = this.apiUrl + `/${data}`;
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    return fetch(url, options)
+      .then(response => response.json())
+      .catch(err => err);
+  }
+}
