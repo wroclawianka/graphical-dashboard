86fe1ce71eb32c38576922368096d07c1800905c
wroclawianka <dorota.zelga@gmail.com>
2018-09-14 15:00:50 +0200

    helpers tests for countPercentage, sumValues and formtValues

--- spec/HelpersSpec.js
+++ spec/HelpersSpec.js
@@ -2,7 +2,10 @@ import {
   formatNumber,
   formatNumberValues,
   formatMonetaryValue,
-  formatMonetaryValues
+  formatMonetaryValues,
+  countPercentage,
+  sumValues,
+  formatValue
 } from "../js/views/helpers";
 
 describe("Helpers", function() {
@@ -84,6 +87,68 @@ describe("Helpers", function() {
       const values = [80000, 120000];
       const expected = ["80.000 €", "120.000 €"];
       expect(formatMonetaryValues(values)).toEqual(expected);
+    });
+  });
+
+  it("should count percentage", function() {
+    const total = 15000;
+    const value = 1500;
+    const expected = "10%";
+    expect(countPercentage(total, value)).toBe(expected);
+  });
+
+  it("should sum values", function() {
+    const values = [15000, 2500, 100, 50];
+    const value = 2500;
+    const expected = 17650;
+    expect(sumValues(values)).toBe(expected);
+  });
+
+  // format value
+  describe("when value is monetary", function() {
+    const isMonetaryValue = true;
+    it("should be formatted as monetary value", function() {
+      const value = 100000;
+      const expected = "100.000 €";
+      expect(formatValue(value, isMonetaryValue)).toBe(expected);
+    });
+
+    describe("and additional parameters is specified", function() {
+      const format = "en-US";
+      const currency = "$";
+      it(`should be formatted with ${format}`, function() {
+        const value = 100000;
+        const expected = "100,000 €";
+        expect(formatValue(value, isMonetaryValue, format)).toBe(expected);
+      });
+
+      it(`should be formatted with ${format} and currency symbol`, function() {
+        const value = 100000;
+        const expected = `100,000 ${currency}`;
+        expect(formatValue(value, isMonetaryValue, format, currency)).toBe(
+          expected
+        );
+      });
+    });
+  });
+
+  // format value
+  describe("when value is number", function() {
+    const isMonetaryValue = false;
+    it("should formatted as number", function() {
+      const value = 100000;
+      const expected = "100.000";
+      expect(formatValue(value)).toBe(expected);
+    });
+
+    describe("and additional parameters is specified", function() {
+      const format = "en-US";
+
+      it(`should be formatted with ${format}`, function() {
+        const value = 100000;
+        const expected = "100,000";
+        expect(formatValue(value, isMonetaryValue, format)).toBe(expected);
+      });
     });
   });
 });
